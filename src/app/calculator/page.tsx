13f053a69d35bacd2b4d8b8be5aca62fbf350d72
Khalil Meqqori <khalilmeqqori@Mac-mini-de-Khalil.local>
2024-12-30 17:11:07 +0100

    fix linter

--- src/app/calculator/page.tsx
+++ src/app/calculator/page.tsx
@@ -1,18 +1,11 @@
+
 'use client';
 import React from 'react';
 import WithAuth from '@/components/hoc/withAuth';
-import { ReactCalculator } from 'react-calculator-ts';
 
 function Calculator() {
 	return (
 		<div style={{ padding: '64px' }}>
-			<ReactCalculator
-				type={'scientific'}
-				numberButtonColor={'#4b5563'}
-				operationButtonColor={'#ef4444'}
-				equalButtonColor={'#10b981'}
-				clearButtonColor={'#f59e0b'}
-			/>
 		</div>
 	);
 }
